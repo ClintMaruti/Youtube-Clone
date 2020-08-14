6804effaef7792976d046eb090e896e718f03489
Mercy Ogutu <Mercy@Mercys-MacBook-Air.local>
2020-08-14 17:45:49 +0300

    [initial commit]

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Grid } from "@material-ui/core";
+import { SearchBar, VideoDetails, VideoList } from "./components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import youtube from "./api/youtube";
+
+class App extends React.Component {
+  state = {
+    videos: [],
+    selectedVideos: null,
+  };
+
+  componentDidMount() {
+    this.handleSubmit("Sauti Sol");
+  }
+  handleSubmit = async (searchTerm) => {
+    const response = await youtube.get("search", {
+      params: {
+        part: "snippet",
+        maxResults: 5,
+        key: "AIzaSyCq5UT2K-PSyANUbv2R2pF-ymfFhNah8as",
+        q: searchTerm,
+      },
+    });
+    this.setState({
+      videos: response.data.items,
+      selectedVideos: response.data.items[0],
+    });
+  };
+
+  onVideoSelect = (video) => {
+    this.setState({
+      selectedVideos: video,
+    });
+  };
+  render() {
+    const { selectedVideos, videos } = this.state;
+    return (
+      <Grid justify="center" container spacing={10}>
+        <Grid item xs={12}>
+          <Grid container spacing={10}>
+            <Grid item xs={12}>
+              <SearchBar onFormSubmit={this.handleSubmit} />
+            </Grid>
+            <Grid item xs={8}>
+              <VideoDetails video={selectedVideos} />
+            </Grid>
+            <Grid item xs={4}>
+              <VideoList videos={videos} onVideoSelect={this.onVideoSelect} />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    );
+  }
 }
 
 export default App;
---
+++ src/api/youtube.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+// AIzaSyCq5UT2K-PSyANUbv2R2pF-ymfFhNah8as
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+});
